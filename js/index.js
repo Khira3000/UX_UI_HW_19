8bb39e7853c738cd49f93e9c4b830d96670b12b4
Khira3000 <142181706+Khira3000@users.noreply.github.com>
2024-01-21 18:08:11 +0000

    added accordion

---
+++ js/index.js
@@ -0,0 +1,20 @@
+console.log ("js file loaded correctly"); 
+
+
+const accordions = document.querySelectorAll('.accordion');
+accordions.forEach(accordion => {
+  accordion.addEventListener('click', e => {
+    let accordionBtn = accordion.querySelector('button');
+    accordion.classList.toggle('active');
+    if (accordionBtn.textContent === '-') {
+      accordionBtn.textContent = '+';
+    } else {
+      accordionBtn.textContent = '-';
+    }
+
+
+  })
+
+
+
+})
